07c98f909caa611a0f89e4d981bf3aa87e1448ef
Arialdo Martini <arialdomartini@gmail.com>
2016-05-24 23:25:18 +0200

    Playing with call-by-name and call-by-value

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,16 @@
-
 object Main {
+  def callByValue(i: Int, b: Int) = i
+  def callByName(i: Int, b: => Int) = i
+  def loop() : Int = loop()
+
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println("Evaluating by name or by value")
+
+    println("Call by name: ")
+    println( callByName(99, loop()))
+
+    println("Call by value (won't terminate): ")
+    println( callByValue(99, loop()))
+    
   }
 }
