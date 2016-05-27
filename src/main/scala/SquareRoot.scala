e9a485489e36d113f48a60fa2806802653a4117e
Arialdo Martini <arialdomartini@gmail.com>
2016-05-27 08:17:03 +0200

    Leveraging lexical scope in nested functions

--- src/main/scala/SquareRoot.scala
+++ src/main/scala/SquareRoot.scala
@@ -1,14 +1,14 @@
 object SquareRoot {
   def calculate(x: Double): Double = 
   {
-    def calculate(guess: Double, x: Double): Double = 
-      if(isGoodEnough(guess, x)) guess
-      else calculate(improve(guess, x), x)
+    def calculateIter(guess: Double): Double = 
+      if(isGoodEnough(guess)) guess
+      else calculateIter(improve(guess))
 
-    def isGoodEnough(guess: Double, x: Double) = Math.abs(guess * guess - x)/x < 0.001
-    def improve(guess: Double, x: Double) = (guess + x/guess) /2
+    def isGoodEnough(guess: Double) = Math.abs(guess * guess - x)/x < 0.001
+    def improve(guess: Double) = (guess + x/guess) /2
 
-    calculate(1.0, x) 
+    calculateIter(1.0)
   }
 
 }
