375dec36dd62ab305feecadf3d685efb375f0a0e
Arialdo Martini <arialdomartini@gmail.com>
2016-05-27 08:12:57 +0200

    Using nested functions

--- src/main/scala/SquareRoot.scala
+++ src/main/scala/SquareRoot.scala
@@ -1,11 +1,15 @@
 object SquareRoot {
-  def calculate(x: Double): Double = calculate(1.0, x)
+  def calculate(x: Double): Double = 
+  {
+    def calculate(guess: Double, x: Double): Double = 
+      if(isGoodEnough(guess, x)) guess
+      else calculate(improve(guess, x), x)
 
-  def calculate(guess: Double, x: Double): Double = 
-    if(isGoodEnough(guess, x)) guess
-    else calculate(improve(guess, x), x)
+    def isGoodEnough(guess: Double, x: Double) = Math.abs(guess * guess - x)/x < 0.001
+    def improve(guess: Double, x: Double) = (guess + x/guess) /2
 
- def isGoodEnough(guess: Double, x: Double) = Math.abs(guess * guess - x)/x < 0.001
- def improve(guess: Double, x: Double) = (guess + x/guess) /2
+    calculate(1.0, x) 
+  }
+
 }
 
