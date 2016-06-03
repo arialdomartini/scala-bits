08eb8d5e903d2d0afd8bc4697bb417ac7aeafd19
Arialdo Martini <arialdomartini@gmail.com>
2016-06-03 05:41:01 +0200

    sum and product using hhl and currying

--- src/main/scala/HHF.scala
+++ src/main/scala/HHF.scala
@@ -4,15 +4,12 @@ object HHF {
   def product(a: Int, b: Int): Int = a * b
 
 
-  def sumRange = map(sum, 0)
+  def sumRange = map(sum, 0) _
 
-  def productRange = map(product, 1)
+  def productRange = map(product, 1) _
 
-  def map(collant: (Int, Int) => Int, invariant: Int): (Int, Int) => Int = {
-    def genericMap(from: Int, to: Int): Int = {
-      if(from > to) invariant
-      else collant(from , genericMap(from + 1, to))
-    }
-    genericMap
+  def map(collant: (Int, Int) => Int, invariant: Int)(from: Int, to: Int): Int = {
+    if(from > to) invariant
+    else collant(from , map(collant, invariant)(from + 1, to))
   }
 }
