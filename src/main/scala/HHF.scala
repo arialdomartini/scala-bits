6b0a265b8a0671866a6d0ebc27982aee930a4e0a
Arialdo Martini <arialdomartini@gmail.com>
2016-06-03 07:09:17 +0200

    Using anonymous functions

--- src/main/scala/HHF.scala
+++ src/main/scala/HHF.scala
@@ -1,18 +1,15 @@
 object HHF {
   def itself: Int => Int = x => x
   def double(n: Int) = 2 * n
-  def sum(a: Int, b: Int): Int = a + b
-  def product(a: Int, b: Int): Int = a * b
 
+  def sumRange = map((a, b) => a + b, 0, itself) _
 
-  def sumRange = map(sum, 0, itself) _
+  def productRange = map((a, b) => a * b, 1, itself) _
 
-  def productRange = map(product, 1, itself) _
+  def sumDoublesRange = map((a, b) => a + b, 0, double) _
 
-  def sumDoublesRange = map(sum, 0, double) _
-
-  def map(collant: (Int, Int) => Int, invariant: Int, f: Int => Int)(from: Int, to: Int): Int = {
+  def map(combine: (Int, Int) => Int, invariant: Int, f: Int => Int)(from: Int, to: Int): Int = {
     if(from > to) invariant
-    else collant(f(from) , map(collant, invariant, f)(from + 1, to))
+    else combine(f(from) , map(combine, invariant, f)(from + 1, to))
   }
 }
