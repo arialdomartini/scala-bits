fe847755615942864116ba2d6ae353bde07490cb
Arialdo Martini <arialdomartini@gmail.com>
2016-06-07 07:56:27 +0200

    playing with binary trees

---
+++ src/main/scala/Classes.scala
@@ -0,0 +1,22 @@
+abstract class IntSet {
+  def contains(elem: Int): Boolean
+  def include(elem: Int): IntSet
+}
+
+class Empty extends IntSet {
+  def contains(elem: Int): Boolean = false
+  def include(elem: Int): IntSet = new NotEmpty(elem, new Empty, new Empty)
+}
+
+class NotEmpty(value: Int, left: IntSet, right: IntSet) extends IntSet {
+  def contains(elem: Int): Boolean = {
+    if(elem < value) left.contains(elem)
+    else if(elem > value) right.contains(elem)
+    else true
+  }
+  def include(elem: Int): IntSet = {
+    if(elem < value) new NotEmpty(value, left include elem, right)
+    else if(elem > value) new NotEmpty(value, left, right include elem)
+    else this
+  }
+}
---
+++ src/test/scala/ClassesTest.scala
@@ -0,0 +1,40 @@
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ClassesTest extends FunSuite {
+  test("empty does not contain anything") {
+    val sut = new Empty
+    
+    assert(sut.contains(10) === false)
+  }
+
+  test("a not empty element contains its own value") {
+    val sut = new NotEmpty(99, new Empty, new Empty)
+
+    assert(sut.contains(99) === true)
+    assert(sut.contains(42) === false)
+  }
+
+  test("a not empty element contains a value if its children contains it") {
+    val left = new NotEmpty(1, new Empty, new Empty)
+    val right = new NotEmpty(100, new Empty, new Empty)
+    val sut = new NotEmpty(50, left, right)
+
+    assert(sut.contains(1) === true)
+    assert(sut.contains(100) === true)
+  }
+
+  test("an intset can include new intsets") {
+    val sut: IntSet = new NotEmpty(100, new Empty, new Empty)
+
+    assert(sut.contains(100) === true)
+    assert(sut.contains(50) === false)
+
+    val result = sut.include(50)
+
+    assert(result.contains(100) === true)
+    assert(result.contains(50) === true)
+  }
+}
