8798b2d4973997860059f4828a61383e746f58e0
Arialdo Martini <arialdomartini@gmail.com>
2016-06-27 00:02:03 +0200

    Calculating scalar product of vectors

--- src/main/scala/Collections.scala
+++ src/main/scala/Collections.scala
@@ -2,4 +2,6 @@ object Collections {
   def combinations(m: Int, n: Int): scala.collection.immutable.IndexedSeq[(Int, Int)] = {
     (1 to m).flatMap(x => (1 to n).map(y => (x, y)))
   }
+
+  def scalarProduct(v1: Vector[Double], v2: Vector[Double]): Double = (v1 zip v2).map( pair => pair._1 * pair._2 ).sum
 }
--- src/test/scala/CollectionsTest.scala
+++ src/test/scala/CollectionsTest.scala
@@ -4,10 +4,22 @@ import org.scalatest.junit.JUnitRunner
 import Collections._
 
 @RunWith(classOf[JUnitRunner])
-class CollectionsTest extends FlatSpec with Matchers {
+class CollectionsTest extends FlatSpec with Matchers with GivenWhenThen {
   it should "list all combinations of numbers x and y where x is drawn from 1..M and y is drawn from 1..N" in {
     val result = combinations(2, 2)
 
     result should contain theSameElementsAs Vector((1,1), (1,2), (2,1), (2,2))
+  }
+
+  it should "calculate scalar product of two vectors" in {
+    Given("two vectors")
+    val v1: Vector[Double] = Vector(1, 2, 3)
+    val v2: Vector[Double] = Vector(4, 5, 6)
+
+    When("scalar product is calculated")
+    val result = scalarProduct(v1, v2)
+
+    Then("the result should be the sum of the products of each pair")
+    result should be (1 * 4 + 2 * 5 + 3 * 6)
   }
 }
